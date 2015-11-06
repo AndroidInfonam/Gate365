38c29965a92362c73029c99fc42d329efaea9b36
Duong Vo <duong.vo@infonam.com>
2015-11-06 15:01:58 +0700

    [init] construct project.

---
+++ app/src/test/java/gate365/infonam/com/gate365/ExampleUnitTest.java
@@ -0,0 +1,15 @@
+package gate365.infonam.com.gate365;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() throws Exception {
+        assertEquals(4, 2 + 2);
+    }
+}

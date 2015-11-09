4a25a7b9e58a652ddc00dfb4fa0b1c6118334785
chqthang <thang.cai@infonam.com>
2015-11-09 17:25:03 +0700

    [thang] add login screen.

--- app/src/main/java/gate365/infonam/com/gate365/Login.java
+++ app/src/main/java/gate365/infonam/com/gate365/Login.java
@@ -7,17 +7,18 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class Gate365Activity extends AppCompatActivity {
-    Button call;
+public class Login extends AppCompatActivity {
+    Button Login;
+    EditText user,pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_gate365);
+        setContentView(R.layout.activity_login);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -29,35 +30,19 @@ public class Gate365Activity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-        call=(Button)findViewById(R.id.button2);
-        call.setOnClickListener(new View.OnClickListener() {
+        Login=(Button)findViewById(R.id.button);
+        user=(EditText)findViewById(R.id.editText);
+        pass=(EditText)findViewById(R.id.editText2);
+        Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                finish();
+                if(user.getText().toString().equals("admin")&&pass.getText().toString().equals("admin")) {
+                    Intent i = new Intent(Login.this, Gate365Activity.class);
+                    startActivity(i);
+                }
+                else Toast.makeText(getApplication(),"error login",Toast.LENGTH_SHORT).show();
             }
         });
-
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_gate365, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
